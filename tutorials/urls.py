88ead56a1a6f0094af956a7499a5b772d2296f36
Prarthana K <prarthana_k@outlook.com>
2025-02-19 17:22:26 -0800

    first commit

---
+++ tutorials/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from tutorials import views as tutorials_views
+from django.conf.urls.static import static
+from django.conf import settings
+from django.urls import reverse
+import pytest
+
+urlpatterns = [
+    path('', tutorials_views.index.as_view(), name='home'),
+    path('api/tutorials/', tutorials_views.tutorial_list),
+    path('api/tutorials/<int:pk>/', tutorials_views.tutorial_detail),
+    path('api/tutorials/published/', tutorials_views.tutorial_list_published)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
